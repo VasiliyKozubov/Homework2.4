58d43b6992cd6f6a87332cc9acd502cc020a0b10
Vasiliy_Kozubov` <kozubov27@gmail.com`>
2023-04-11 21:55:03 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// let a = 1;
+
+// while (a <= 2){
+//     console.log('Привет');
+//     a++;
+// }
+
+
+
+// let b = 0;
+// let c = 1;
+// while (c <= 5) {
+//     b = c++;
+//     console.log(b);
+// }
+
+
+
+
+// let d = 0;
+// let e = 7;
+// while (e <= 22) {
+//     d = e++;
+//     console.log(d);
+// }
+
+
+
+// const obj = {
+//     'Коля': 200,
+//     'Вася': 300,
+//     'Петя': 400,
+// };
+
+// for (const key in obj) {
+
+
+//     alert( key + " — зарпалата " + obj[key] + ' долларов ');
+// }
+
+
+
+// let n = 1000;
+
+// let num = 2**5;
+
+
+
